fda902f31e4807c1a69c2edf5dcf070a6194824e
haru-lab-pc <ha.brack96@gmail.com>
2025-08-11 16:38:10 +0900

    :feat: 行番号など基本的な設定を変更

--- init.lua
+++ init.lua
@@ -1,8 +1,22 @@
--- key map
+local opt = vim.opt
 
+-- 行番号の表示
+opt.number = true
+
+-- タブとインデントの設定
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+-- 検索設定
+opt.ignorecase = true -- 通常は検索時に大文字小文字を区別しない
+opt.smartcase = true  -- 大文字が含まれていたら大文字小文字を区別する
+
+-- key map
 -- -- ファイルタブ切替
 vim.keymap.set("n", "<C-h>", "<cmd>bprev<CR>")
 vim.keymap.set("n", "<C-l>", "<cmd>bnext<CR>")
+
 
 -- lazy
 require("config.lazy")
